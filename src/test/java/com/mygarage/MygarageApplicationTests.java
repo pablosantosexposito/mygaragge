104289eed2e176ab372867ffaa7b1d9ecff6df2b
DESKTOP-RMQ2EM8\Pablo <pablosantosexposito@gmail.com>
2023-09-13 19:00:53 +0200

    feature-1:initialization first commit

---
+++ src/test/java/com/mygarage/MygarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
